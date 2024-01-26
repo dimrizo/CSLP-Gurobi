f39eb60084206b2dfca4a9a34c9fa525a58ee840
Dimitris Rizopoulos <dimrizopoulos@gmail.com>
2024-01-26 11:01:12 +0200

    reverse N1 & N2 to match manuscript formulation

--- EB-CSLP v1.py
+++ EB-CSLP v1.py
@@ -6,29 +6,29 @@ from gurobipy import GRB
 import haversine
 
 # example
-n = 14
-v = 7
-m = 10
-k = 3
-f1 = 10 # fast
-f2 = 5 # slow
+n = 14 # Number of charging options
+v = 7 # Number of candidate locations for charging stations
+m = 10 # Total number of bus lines in the problem
+k = 3 # Number of charging lines in the problem
+f1 = 5 # Number of charging slots for SLOW chargers (less since one charging slot occupies more hours in a day)
+f2 = 10 # Number of charging slots for FAST chargers (more since they refer to smaller time intervals)
 theta = {1:1, 2:1, 3:2, 4:2, 5:3, 6:3, 7:4, 8:4, 9:5, 10:5, 11:6, 12:6, 13:7, 14:7} # N -> V
 BigM = 100000
 
 # SETS
 N = [i for i in range(1, n+1)] # set of all possible station installation options
-N1 = N[::2] # fast
-N2 = N[1::2] # slow
+N1 = N[::2] # Indices for SLOW chargers
+N2 = N[1::2] # Indices for FAST chargers
 V = [i for i in range(1, v+1)] # set of all possible charging station physical locations
 M = [i for i in range(1, m+1)] # set of all bus trips
 K = [i for i in range(1, k+1)] # set of all trips that need charging
-F1 = [i for i in range(1, f1+1)] # set of fast charging time slots
-F2 = [i for i in range(1, f2+1)] # set of slow charging time slots
-charging_slots_starting_times_fast = {i:(540 + i * 60) for i in F1}
-charging_slots_starting_times_slow = {i:(540 + i * 120) for i in F2}
+F1 = [i for i in range(1, f1+1)] # set of SLOW charging time slots
+F2 = [i for i in range(1, f2+1)] # set of FAST charging time slots
 
-#print(charging_slots_starting_times)
-tau = {1: 2, 2: 3, 3: 5}
+# assuming continuous time reprsentation for a daily schedule 0-1440
+charging_slots_starting_times_slow = {i:(480 + i * 120) for i in F1} # Here we assume continuous time representation. We consider that \
+charging_slots_starting_times_fast = {i:(540 + i * 60) for i in F2}  # fast charging slots to have 60 min duration and slow have 120 min. 
+tau = {1: 610, 2: 660, 3: 710}
 
 print("Set N: ", N)
 print("Set N1: ", N1)
@@ -39,9 +39,9 @@ print("Set K: ", K)
 print("Set F1: ", F1)
 print("Set F2: ", F2)
 
-# assuming continuous time reprsentation for a daily schedule 0-1440
-#print("Charging_slots_starting_time:", charging_slots_starting_times)
-print("tau:",tau)
+print("Charging slots starting times for SLOW chargers:", charging_slots_starting_times_slow)
+print("Charging slots starting times for FAST chargers:", charging_slots_starting_times_fast)
+print("tau:", tau)
 
 # Parameters
 SOC = {}
@@ -76,7 +76,7 @@ for k in K:
 
 # Average bus speed for urban environments extracted from:
 # https://www.researchgate.net/publication/272687997_Energy_and_Environmental_Impacts_of_Urban_Buses_and_Passenger_Cars-Comparative_Analysis_of_Sensitivity_to_Driving_Conditions/figures?lo=1
-avg_u = 26000 /60
+avg_u = 26000/60
 
 t = {}
 for k in K:
@@ -84,12 +84,12 @@ for k in K:
         t[(k, j)] = d[(k, j)] / avg_u
 
 #Printing travel times dictionary
-print("\n")
-print("Printing travel times matrix in minutes: ")
-for k in K:
-    for j in N:
-        print("(", k, ",", j, "):", round(t[(k, j)], 2),  end=", ")
-    print("\r")
+# print("\n")
+# print("Printing travel times matrix in minutes: ")
+# for k in K:
+#     for j in N:
+#         print("(", k, ",", j, "):", round(t[(k, j)], 2),  end=", ")
+#     print("\r")
 
 a = {}
 for i in M:
@@ -113,11 +113,11 @@ consumption_e = 0.00074 # in kWh/meter (apo th texnikh ekthesi "Yphresies aksiol
 model = gp.Model()
 
 # Variables
-x = model.addVars(N, vtype=GRB.BINARY, name= "Xj")
-q = model.addVars(K, N, vtype=GRB.BINARY, name="Qkj")
-u_fast = model.addVars(K, N, F1, vtype=GRB.BINARY, name="U_fastkjf")
-u_slow = model.addVars(K, N, F2, vtype=GRB.BINARY, name="U_slowkjf")
-y = model.addVars(K, vtype=GRB.CONTINUOUS, name="Yk")
+x = model.addVars(N, vtype=GRB.BINARY, name= "X")
+q = model.addVars(K, N, vtype=GRB.BINARY, name="Q")
+u_slow = model.addVars(K, N, F1, vtype=GRB.BINARY, name="U_slow")
+u_fast = model.addVars(K, N, F2, vtype=GRB.BINARY, name="U_fast")
+y = model.addVars(K, vtype=GRB.CONTINUOUS, name="Y")
 
 # Constraints
 model.addConstrs(sum(a[i, j] * x[j] for j in N) - 1 >= 0 for i in M) # Constraint (4)
@@ -126,28 +126,12 @@ model.addConstr(sum(x[j] * b[j] for j in N) <= B) # Constraint (6)
 model.addConstrs(q[k, j] <= x[j] for k in K for j in N) # Constraint (7)
 model.addConstrs(sum(q[k, j] for j in N) == 1 for k in K) # Constraint (8)
 
-model.addConstrs(sum(u_fast[k, j, f] for f in F1) + sum(u_slow[k, j, f] for f in F2) <= q[k, j] for k in K for j in N) # new constraint
+model.addConstrs(sum(u_slow[k, j, f] for f in F1) + sum(u_fast[k, j, f] for f in F2) <= q[k, j] for k in K for j in N) # new constraint
 
-model.addConstrs(sum(sum(u_fast[k, j, f] for f in F1) for j in N) + sum(sum(u_slow[k, j, f] for f in F2) for j in N) == 1 for k in K) # Constraint (9)
+model.addConstrs(sum(sum(u_slow[k, j, f] for f in F1) for j in N1) + sum(sum(u_fast[k, j, f] for f in F2) for j in N2) == 1 for k in K) # New constraint (9)
 
-#model.addConstrs(u[k, j, f] - u[k, j, f+1] <= u[k, j, f-1] for j in N1 for k in K for f in F if f != 10 if f != 1) # new constraint
-
-#model.addConstrs(u[k, j, f] - u[k, j, f+1] == 0 for j in N1 for k in K for f in F if f == 1 if f != 10) # new constraint
-
-# prepei an u[k, j ,f] == 1 gia j anhkei sto N1, tote u[k, j ,f+1] == 1
-
-# for k in K:
-#     for j in N1:
-#         for f in F:
-#             if f != 10:
-
-#model.addConstrs(sum(sum(u[k, j, f] + u[k, j, f+1] for f in F if f != 10) for j in N1) == 2 for k in K) # Constraint (10) mod
-#model.addConstrs(sum(sum(u[k, j, f] for f in F) for j in N) <= 2 for k in K)
-
-#model.addConstrs(sum(sum(u[k, j, f] + u[k, j, f+1] for f in F if f != 10) for j in N1) <= 4 for k in K) # Constraint (10) mod
-
-model.addConstrs(sum(u_fast[k, j, f] for k in K) <= 1 for j in N1 for f in F1) # Constraint (11)
-model.addConstrs(sum(u_slow[k, j, f] for k in K) <= 1 for j in N2 for f in F2)
+model.addConstrs(sum(u_slow[k, j, f] for k in K) <= 1 for j in N1 for f in F1) # New constraint (11Α)
+model.addConstrs(sum(u_fast[k, j, f] for k in K) <= 1 for j in N2 for f in F2) # New constraint (11Β)
 
 model.addConstrs((SOC[k] - consumption_e * q[k, j] * d[k, j]) >= SOC_min for k in K for j in N) # Constraint (12)
 
@@ -157,11 +141,8 @@ for j in N:
         if (j != r) & (theta[j] == theta[r]):
             model.addConstr(x[j] + x[r] <= 1)
 
-model.addConstrs((1-u_fast[k, j, f])*BigM + u_fast[k, j, f] * charging_slots_starting_times_fast[f] >= (tau[k] + t[k, j]) * q[k, j] for k in K for j in N1 for f in F1) # Constraint (14)
-model.addConstrs((1-u_slow[k, j, f])*BigM + u_slow[k, j, f] * charging_slots_starting_times_slow[f] >= (tau[k] + t[k, j]) * q[k, j] for k in K for j in N2 for f in F2)
-
-# test constraint
-# model.addConstrs(u[k, j, f+1] - u[k, j, f] == 0 for k in K for j in N1 for f in F if f != 10) # Constraint (10)
+model.addConstrs((1-u_slow[k, j, f])*BigM + u_slow[k, j, f] * charging_slots_starting_times_slow[f] >= (tau[k] + t[k, j]) * q[k, j] for k in K for j in N1 for f in F1) # Constraint (14Α)
+model.addConstrs((1-u_fast[k, j, f])*BigM + u_fast[k, j, f] * charging_slots_starting_times_fast[f] >= (tau[k] + t[k, j]) * q[k, j] for k in K for j in N2 for f in F2) # Constraint (14Β)
 
 model.setObjective(sum(y[k] for k in K), GRB.MINIMIZE)
 model.optimize()
