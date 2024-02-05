574f5e9e696182bb447ccb1eaecf10c8bc28a26d
Dimitris Rizopoulos <dimrizopoulos@gmail.com>
2024-02-05 16:18:13 +0200

    first transfer of logic to synth data creator

--- EB-CSLP V4.py
+++ EB-CSLP V4.py
@@ -3,53 +3,42 @@
 
 import gurobipy as gp
 from gurobipy import GRB
+import json
 
 import haversine
 import synth_data_kreator
 
+# Calling the synthetic data creation module to get EB-CSLP problems
 problems = synth_data_kreator.main()
-
-print(problems)
-
-# Example parameters data
-n     = 4       # Number of charging options
-v     = 4       # Number of candidate locations for charging stations
-m     = 50      # Total number of bus lines in the problem
-k     = 10      # Number of charging lines in the problem, den epanalamvanontai
-f1    = 6       # Number of charging slots for SLOW chargers (less since one charging slot occupies more hours in a day)
-f2    = 12      # Number of charging slots for FAST chargers (more since they refer to smaller time intervals)
-big_M = 100000  # A big number M
+print(json.dumps(problems, indent=4))
 
 # Sets
-N     = [i for i in range(1, n+1)] # set of all possible station installation options
-N1    = [1, 2] # Charging option indices for SLOW chargers
-N2    = [3, 4] # Charging option indices for FAST chargers
-theta = {1:1, 2:2, 3:3, 4:4} # N -> V
+N     = problems[1]["CS"]["N"]
+N1    = problems[1]["CS"]["N1"] # Charging option indices for SLOW chargers
+N2    = problems[1]["CS"]["N2"] # Charging option indices for FAST chargers
+theta = problems[1]["CS"]["theta"]
+b = problems[1]["CS"]["b"]
 
-V  = [i for i in range(1, v+1)] # set of all possible charging station physical locations
-M  = [i for i in range(1, m+1)] # set of all bus trips
-K  = [i for i in range(1, k+1)] # set of all trips that need charging
-F1 = [i for i in range(1, f1+1)] # set of SLOW charging time slots
-F2 = [i for i in range(1, f2+1)] # set of FAST charging time slots
+V  = problems[1]["CS"]["V"] # set of all possible charging station physical locations
+M  = problems[1]["bus"]["M"] # set of all bus trips
+K  = problems[1]["bus"]["K"] # set of all trips that need charging
+F1 = problems[1]["time"]["F1"] # set of SLOW charging time slots
+F2 = problems[1]["time"]["F2"] # set of FAST charging time slots
 
 # assuming continuous time reprsentation for a daily schedule 0-1440
-charging_slots_starting_times_slow = {i:(480 + i * 120) for i in F1} # Here we assume continuous time representation. We consider that \
-charging_slots_starting_times_fast = {i:(540 + i * 60) for i in F2}  # fast charging slots to have 60 min duration and slow have 120 min.
-tau = {1: 610, 2: 660, 3: 710, 4: 740, 5: 810, 6: 890, 7:910, 8:1000, 9:1010, 10:1050}
-# pk = {1:710, 2:760, 3:810, 4:840, 5:910, 6:990, 7:1010, 8:1100, 9:1110, 10:1150} #xronos meta thn fortisi
-# pk = {1: 680, 2: 730, 3: 780, 4: 810, 5: 880, 6: 960, 7:980, 8:1070, 9:1080, 10:1120} #xronos meta thn fortisi
-pk = {i:(tau[i] + 87) for i in tau}
+charging_slots_slow = problems[1]["time"]["charging_slots_slow"] # Here we assume continuous time representation. We consider that \
+charging_slots_fast = problems[1]["time"]["charging_slots_fast"]  # fast charging slots to have 60 min duration and slow have 120 min.
+tau = problems[1]["time"]["tau"]
+pk = problems[1]["time"]["pk"]
 
 # Parameters
-SOC = {}
-for k in K:
-    SOC[k] = 100 # in kWh
-SOC_min = 20 # in kWh
+SOC = problems[1]["bus"]["SOC"]
+SOC_min = problems[1]["bus"]["SOC_min"] # in kWh
 
-tcK = {1:37.9718, 2:37.9812, 3:38.0355, 4:37.9828, 5:38.0455, 6:37.9712, 7:38.0585, 8:37.9822, 9:37.9612, 10:38.0555}
-tyK = {1:23.7816, 2:23.7345, 3:23.7695, 4:23.7716, 5:23.7445, 6:23.7685, 7:23.7225, 8:23.7595, 9:23.7316, 10:23.7795}
-tcV = {1:37.9733, 2:38.0012, 3:38.0088, 4:37.9932}
-tyV = {1:23.6689, 2:23.6737, 3:23.7629, 4:23.7930}
+tcV = problems[1]["CS"]["tcV"]
+tyV = problems[1]["CS"]["tyV"]
+tcK = problems[1]["bus"]["tcK"]
+tyK = problems[1]["bus"]["tyK"]
 
 # Printing coordinates
 # print("\n")
@@ -57,6 +46,11 @@ tyV = {1:23.6689, 2:23.6737, 3:23.7629, 4:23.7930}
 # print(tyK)
 # print(tcV)
 # print(tyV)
+
+big_M = problems[1]["aux"]["big_M"]
+avg_u = problems[1]["aux"]["avg_u"]
+consumption_e = problems[1]["aux"]["consumption_e"]
+total_B = problems[1]["aux"]["total_B"]
 
 # Calculating distances
 d = {}
@@ -71,10 +65,6 @@ for k in K:
 #     for j in N:
 #         print("(", k, ",", j, "):", round(d[(k, j)], 2),  end=", ")
 #     print("\r")
-
-# Average bus speed for urban environments extracted from:
-# https://www.researchgate.net/publication/272687997_Energy_and_Environmental_Impacts_of_Urban_Buses_and_Passenger_Cars-Comparative_Analysis_of_Sensitivity_to_Driving_Conditions/figures?lo=1
-avg_u = 26000/60
 
 t = {}
 for k in K:
@@ -102,11 +92,6 @@ for i in M:
 #         print("(", k, ",", j, "):", a[(k, j)],  end=", ")
 #     print("\r")
 
-B = 1000000
-b = {1:700, 2:750, 3:500, 4:550, 5:600, 6:650, 7:900, 8:950}
-
-consumption_e = 0.00074 # in kWh/meter (apo th texnikh ekthesi "Yphresies aksiologhshs programmatos pilotikhs kyklloforias hlektrikon leoforeion"
-
 # Initialize the Gurobi model
 model = gp.Model()
 
@@ -120,7 +105,7 @@ y = model.addVars(K, vtype=GRB.CONTINUOUS, name="Y")
 # Constraints
 model.addConstrs(sum(a[i, j] * x[j] for j in N) - 1 >= 0 for i in M) # Constraint (4)
 model.addConstrs(sum(t[k, j] * q[k, j] for j in N) == y[k] for k in K) # Constraint (5)
-model.addConstr(sum(x[j] * b[j] for j in N) <= B) # Constraint (6)
+model.addConstr(sum(x[j] * b[j] for j in N) <= total_B) # Constraint (6)
 model.addConstrs(q[k, j] <= x[j] for k in K for j in N) # Constraint (7)
 model.addConstrs(sum(q[k, j] for k in K) >= x[j] for j in N) #To ensure that xj does not take value of 1 if no trip is assigned to charging station j
 model.addConstrs(sum(q[k, j] for j in N) == 1 for k in K) # Constraint (8)
@@ -140,11 +125,11 @@ for j in N:
         if (j != r) & (theta[j] == theta[r]):
             model.addConstr(x[j] + x[r] <= 1)
 
-model.addConstrs((1-u_slow[k, j, f])*big_M + u_slow[k, j, f] * charging_slots_starting_times_slow[f] >= (tau[k] + t[k, j]) * q[k, j] for k in K for j in N1 for f in F1) # Constraint (14Α)
-model.addConstrs((1-u_fast[k, j, f])*big_M + u_fast[k, j, f] * charging_slots_starting_times_fast[f] >= (tau[k] + t[k, j]) * q[k, j] for k in K for j in N2 for f in F2) # Constraint (14Β)
+model.addConstrs((1-u_slow[k, j, f])*big_M + u_slow[k, j, f] * charging_slots_slow[f] >= (tau[k] + t[k, j]) * q[k, j] for k in K for j in N1 for f in F1) # Constraint (14Α)
+model.addConstrs((1-u_fast[k, j, f])*big_M + u_fast[k, j, f] * charging_slots_fast[f] >= (tau[k] + t[k, j]) * q[k, j] for k in K for j in N2 for f in F2) # Constraint (14Β)
 
-model.addConstrs(-(1-u_slow[k, j, f])*big_M + u_slow[k, j, f] * charging_slots_starting_times_slow[f] <= (pk[k] + t[k, j]) * q[k, j] for k in K for j in N1 for f in F1) #gia na mhn fortizei poly argotera
-model.addConstrs(-(1-u_fast[k, j, f])*big_M + u_fast[k, j, f] * charging_slots_starting_times_fast[f] <= (pk[k] + t[k, j]) * q[k, j] for k in K for j in N2 for f in F2) #gia na mhn fortizei poly argotera
+model.addConstrs(-(1-u_slow[k, j, f])*big_M + u_slow[k, j, f] * charging_slots_slow[f] <= (pk[k] + t[k, j]) * q[k, j] for k in K for j in N1 for f in F1) #gia na mhn fortizei poly argotera
+model.addConstrs(-(1-u_fast[k, j, f])*big_M + u_fast[k, j, f] * charging_slots_fast[f] <= (pk[k] + t[k, j]) * q[k, j] for k in K for j in N2 for f in F2) #gia na mhn fortizei poly argotera
 
 model.setObjective(sum(y[k] for k in K), GRB.MINIMIZE)
 model.optimize()
@@ -159,8 +144,8 @@ print("Set K: ", K)
 print("Set F1: ", F1)
 print("Set F2: ", F2)
 
-print("Charging slots starting times for SLOW chargers:", charging_slots_starting_times_slow)
-print("Charging slots starting times for FAST chargers:", charging_slots_starting_times_fast)
+print("Charging slots starting times for SLOW chargers:", charging_slots_slow)
+print("Charging slots starting times for FAST chargers:", charging_slots_fast)
 print("tau:", tau)
 print("After charging time limit:", pk)
 
--- synth_data_kreator.py
+++ synth_data_kreator.py
@@ -1,16 +1,79 @@
 # Railways and Transport Laboratory, National Technical University of Athens
 # Charging Stations Location Problem for Electric Buses (EB-CSLP) - Module for synthetic data creation
 
-import haversine
-
 def main():
     """Creates the synthetic data based on input parameters."""
+    v  = 4       # Number of candidate locations for charging stations
+    n  = 4       # Number of charging options
+    m  = 50      # Total number of bus lines in the problem
+    k  = 10      # Number of charging lines in the problem, den epanalamvanontai
+    f1 = 6       # Number of charging slots for SLOW chargers (less since one charging slot occupies more hours in a day)
+    f2 = 12      # Number of charging slots for FAST chargers (more since they refer to smaller time intervals)
+    charging_start_time = 600
+    charging_time_slow = 120
+    charging_time_fast = 60
+    tau = {1: 610, 2: 660, 3: 710, 4: 740, 5: 810, 6: 890, 7:910, 8:1000, 9:1010, 10:1050}
+    charging_window = 87
+    big_M = 100000  # A big number M
+    # Average bus speed for urban environments extracted from:
+    # https://www.researchgate.net/publication/272687997_Energy_and_Environmental_Impacts_of_Urban_Buses_and_Passenger_Cars-Comparative_Analysis_of_Sensitivity_to_Driving_Conditions/figures?lo=1
+    avg_u = 26000/60
+    total_B = 1000000
+    b = {1:700, 2:750, 3:500, 4:550, 5:600, 6:650, 7:900, 8:950}
+    consumption_e = 0.00074 # in kWh/meter (apo th texnikh ekthesi "Yphresies aksiologhshs programmatos pilotikhs kyklloforias hlektrikon leoforeion"
 
-    problems = {"CS":       {},
-                "bus":      {},
-                "time":     {},
-                "matrices": {},
-                "aux":      {}
-    }
+    # Create data for EB-CSLP
+    # Charging Stations (CS) -related parameters
+    V  = [i for i in range(1, v+1)] # set of all possible charging station physical locations
+    N  = [i for i in range(1, n+1)] # set of all possible station installation options
+    N1    = [1, 2] # Charging option indices for SLOW chargers
+    N2    = [3, 4] # Charging option indices for FAST chargers
+    # N1 = N[::2] # Indices for SLOW chargers
+    # N2 = N[1::2] # Indices for FAST chargers
+    theta = {1:1, 2:2, 3:3, 4:4} # N -> V
+    tcV = {}
+    tyV = {}
+
+    ###
+    # We miss the calculation of random coordintes for Athens for the vectors tcV, tyV.
+    ###
+
+    tcV = {1:37.9733, 2:38.0012, 3:38.0088, 4:37.9932}
+    tyV = {1:23.6689, 2:23.6737, 3:23.7629, 4:23.7930}
+
+    # bus-related model parameters
+    M  = [i for i in range(1, m+1)] # set of all bus trips
+    K  = [i for i in range(1, k+1)] # set of all trips that need charging
+    SOC = {}
+    for k in K:
+        SOC[k] = 100 # in kWh
+    SOC_min = 20 # in kWh
+    tcK = {}
+    tyK = {}
+
+    ###
+    # We miss the calculation of random coordintes for Athens for the vectors tcK, tyK.
+    ###
+
+    tcK = {1:37.9718, 2:37.9812, 3:38.0355, 4:37.9828, 5:38.0455, 6:37.9712, 7:38.0585, 8:37.9822, 9:37.9612, 10:38.0555}
+    tyK = {1:23.7816, 2:23.7345, 3:23.7695, 4:23.7716, 5:23.7445, 6:23.7685, 7:23.7225, 8:23.7595, 9:23.7316, 10:23.7795}
+
+    # time-related model parameters
+    F1 = [i for i in range(1, f1+1)] # set of SLOW charging time slots
+    F2 = [i for i in range(1, f2+1)] # set of FAST charging time slots
+    charging_slots_slow = {i:(charging_start_time + (i-1) * charging_time_slow) for i in F1} # Here we assume continuous time representation. We consider that \
+    charging_slots_fast = {i:(charging_start_time + (i-1) * charging_time_fast) for i in F2}  # fast charging slots to have 60 min duration and slow have 120 min.
+    pk = {i:(tau[i] + charging_window) for i in tau}
+
+
+    problems = {1: {"CS":      {"V": V, "N": N, "N1": N1, "N2": N2, "theta": theta, "b": b, "tcV": tcV, "tyV": tyV},
+                    "bus":      {"M": M, "K": K, "SOC": SOC, "SOC_min": SOC_min, "tcK": tcK, "tyK": tyK},
+                    "time":     {"F1": F1, "F2": F2, 
+                                 "charging_slots_slow": charging_slots_slow, 
+                                 "charging_slots_fast": charging_slots_fast,
+                                 "tau": tau, "pk": pk},
+                    "matrices": {},
+                    "aux":      {"big_M": big_M, "avg_u": avg_u, "consumption_e": consumption_e, "total_B": total_B}
+    }}
 
     return problems
